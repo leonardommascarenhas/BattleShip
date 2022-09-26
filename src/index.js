8ace60ba426c5be11ab72a8744950acab4b919f4
leonardo <leonardommascarenhas@gmail.com>
2022-09-25 21:29:14 -0300

    isSunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const ship = (length) => {
   const shipSize = [...Array(length).keys()];
+  const isSunk = () => {
+    if (shipSize.length == 0) return true;
+  };
   return {
     shipSize,
+    isSunk,
   };
 };
 
-console.log(ship(10));
+const a = ship(0);
+console.log(a);
+console.log(a.isSunk());
