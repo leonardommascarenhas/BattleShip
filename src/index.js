381b2dd2265a4c101e1021c90a09ad6815939bc5
leonardo <leonardommascarenhas@gmail.com>
2022-09-26 21:48:50 -0300

    .

--- src/index.js
+++ src/index.js
@@ -48,27 +48,28 @@ const createShip = (append, length) => {
   return a;
 };
 
-const fleet = () => {
-  let fleet = document.querySelector(".fleet");
-  let ship1 = createShip(fleet, 6);
-  let ship2 = createShip(fleet, 5);
-  let ship3 = createShip(fleet, 4);
-  let ship4 = createShip(fleet, 3);
-};
+let fleet = document.querySelector(".fleet");
+let ship1 = createShip(fleet, 6);
+let ship2 = createShip(fleet, 5);
+let ship3 = createShip(fleet, 4);
+let ship4 = createShip(fleet, 3);
 
-fleet();
+console.log(ship1);
+
 gameBoard(12);
 let ships = document.querySelectorAll(".ships");
 let pixels = document.querySelectorAll(".pixel");
 let gameboard = document.querySelector(".gameBoard");
-ships.forEach((ships) => {
-  ships.addEventListener("dragstart", () => {
-    ships.classList.add("dragging");
+
+ships.forEach((ship) => {
+  ship.addEventListener("dragstart", () => {
+    ship.classList.add("dragging");
   });
-  ships.addEventListener("dragend", () => {
-    ships.classList.remove("dragging");
+  ship.addEventListener("dragend", () => {
+    ship.classList.remove("dragging");
   });
 });
+
 pixels.forEach((pixel) => {
   pixel.addEventListener("dragover", (e) => {
     e.preventDefault();
